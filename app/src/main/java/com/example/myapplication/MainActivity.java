5ee4538b30ccf0359b08f4503a9093e1affe9941
HP <namahg2003@gmail.com>
2022-12-14 20:27:14 +0530

    Initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.MyApplication;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import com.google.android.material.button.MaterialButton;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -35,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         assignId(buttonDivide,R.id.button_divide);
         assignId(buttonMultiply,R.id.button_multiply);
         assignId(buttonPlus,R.id.button_plus);
+        assignId(buttonMinus,R.id.button_minus);
+        assignId(buttonEquals,R.id.button_equals);
         assignId(button0,R.id.button_0);
         assignId(button1,R.id.button_1);
         assignId(button2,R.id.button_2);
@@ -45,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         assignId(button7,R.id.button_7);
         assignId(button8,R.id.button_8);
         assignId(button9,R.id.button_9);
+        assignId(buttonAC,R.id.button_ac);
         assignId(buttonDot,R.id.button_dot);
     }
 
@@ -62,8 +67,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
+        if(btnText.equals("=")) {
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
         if(btnText.equals("C")) {
-            toCalculate = toCalculate.substring(0,toCalculate.length()-1);
+            if(!toCalculate.equals("")) {
+                toCalculate = toCalculate.substring(0,toCalculate.length()-1);
+            }
         }
         else {
             toCalculate = toCalculate + btnText;
@@ -79,7 +90,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     String getResult(String data){
         try{
-
+            Context context  = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0","");
             }
             return finalResult;
         }catch (Exception e){
